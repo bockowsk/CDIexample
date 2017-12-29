4de8514e287cf61a7e2245db3ddf5893bc076b23
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2017-12-30 00:22:26 +0100

    CDI test
    test za pomoca Annotations

--- src/main/java/pl/bockowsk/Silownia.java
+++ src/main/java/pl/bockowsk/Silownia.java
@@ -1,8 +1,14 @@
 package pl.bockowsk;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
+
 public class Silownia {
 
-	private Klient jakisGosc = new Cienias();
+	@Inject
+	@Named("Koks")
+	private Klient jakisGosc;
 
 	void otwarcie() {
         jakisGosc.cwiczy();
